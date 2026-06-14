18a06d8be2586a1731eca1070c21dd88321a11a5
shaz-dr24 <shachindranrao@gmail.com>
2026-06-14 20:03:10 +0530

    Configure Upstash Redis and Celery

--- backend/celery_worker.py
+++ backend/celery_worker.py
@@ -1,11 +1,26 @@
+import os
 from celery import Celery
+from dotenv import load_dotenv
+import ssl
+
+load_dotenv()
+
+redis_url = os.getenv("REDIS_URL")
 
 celery_app = Celery(
     "document_tasks",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0",
+    broker=redis_url,
+    backend=redis_url,
     include=[
         "celery_tasks.document_tasks",
         "celery_tasks.email_tasks"
     ]
 )
+
+celery_app.conf.broker_use_ssl = {
+    "ssl_cert_reqs": ssl.CERT_NONE
+}
+
+celery_app.conf.redis_backend_use_ssl = {
+    "ssl_cert_reqs": ssl.CERT_NONE
+}
